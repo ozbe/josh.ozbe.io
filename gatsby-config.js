0793e8facf035eedf6a678c05fcd4e7bfde5103d
ozbe <1372945+ozbe@users.noreply.github.com>
2021-06-12 19:54:21 +1000

    change from bootstrap theme to roboto

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-theme-ui',
       options: {
-        preset: '@theme-ui/preset-bootstrap',
+        preset: '@theme-ui/preset-roboto',
       },
     },
     {
